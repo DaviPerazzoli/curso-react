efebf11fdd699455a4aa4e9872a143b2975e11d6
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-02-22 13:48:47 -0300

    Aprendendo manipulação de formulários

--- arquivos/app01/app03/src/App.js
+++ arquivos/app01/app03/src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 
 export default function App() {
 
-  const carros =['Uno','Corsa','Celta','Gol','Strada','Parati'];
+  const [form,setForm]=useState({nome:'',curso:'',ano:''});
+
+  const handleFormChange=(evt)=>{
+    if(evt.target.getAttribute('name')=='f_nome'){
+      setForm({nome:evt.target.value,curso:form.curso,ano:form.ano});
+    }else if(evt.target.getAttribute('name')=='f_curso'){
+      setForm({nome:form.nome,curso:evt.target.value,ano:form.ano});
+    }else{
+      setForm({nome:form.nome,curso:form.curso,ano:evt.target.value});
+    }
+  }
 
   return (
     <>
-      {carros.map((el)=><p>{el}</p>)}
+      <label>Nome</label>
+      <input type='text' name='f_nome' value={form.nome} onChange={(evt)=>{handleFormChange(evt)}}/><br/>
+      <label>Curso</label>
+      <input type='text' name='f_curso' value={form.curso} onChange={(evt)=>{handleFormChange(evt)}}/><br/>
+      <label>Ano</label>
+      <input type='text' name='f_ano' value={form.ano} onChange={(evt)=>{handleFormChange(evt)}}/>
+      <p>Nome digitado:{form.nome}</p>
+      <p>Curso digitado:{form.curso}</p>
+      <p>Ano digitado:{form.ano}</p>
     </>
   );
 }
