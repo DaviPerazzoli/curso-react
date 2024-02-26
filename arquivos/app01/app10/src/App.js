3e1fda3a3534879ad250b4ed362048defc288066
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-02-26 13:22:18 -0300

    terminei o curso de react, aprendi sobre key

---
+++ arquivos/app01/app10/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React from 'react';
+
+const array_numeros =[1,2,3,4,5,6,7,8,9];
+
+function ListaNumeros(props){
+  const num=props.numeros;
+
+  const lista_numeros=num.map(n=>{
+    return <li key={n.toString()}>{n}</li>
+  })
+
+  return(
+    <ul>
+      {lista_numeros}
+    </ul>
+  )
+}
+
+export default function App() {
+  return (
+    <>
+      <ListaNumeros numeros={array_numeros}/>
+    </>
+  );
+}
---
+++ arquivos/app01/app10/src/componentes/Pagina2.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Pagina2(props){
+    return(
+        <div>
+            <h1>Página 2</h1>
+            <h3>Curso de React</h3>
+        </div>
+    )
+}
