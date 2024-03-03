bb369f9f9ee583b1c2737bda64262e512d08d0b0
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-03-03 18:41:18 -0300

    aprendendo a usar axios

--- arquivos/app01/nome-do-seu-projeto/src/App.jsx
+++ arquivos/app01/nome-do-seu-projeto/src/App.jsx
@@ -1,13 +1,56 @@
 import './App.css'
-import { Outlet } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import NavBar from './components/NavBar';
+import axios from 'axios';
 
 export default function App() {
 
+  const [post, setPost] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(
+    ()=>{
+      const loadPost = () => {
+        setIsLoading(true);
+        axios.get('https://jsonplaceholder.typicode.com/posts/1')
+          .then(response => {
+            setPost(response.data)
+            setIsLoading(false);
+          })
+          .catch(response => {
+            setIsLoading(false);
+            setError(true);
+            console.log(response)
+          })
+      }
+
+      loadPost();
+    },[]
+  );
+  
+  const cretePost = () => {
+    axios.post('https://jsonplaceholder.typicode.com/posts',{
+      title: "Hello World!",
+      body: "This is a new post."
+    })
+      .then((response) => {
+        setPost(response.data);
+      })
+  }
+
+  if(error) {
+    return <p>Error</p>
+  } else if(isLoading) return <p>Loading</p>
+ 
+
   return (
     <div className='App'>
-      <NavBar />
-      <Outlet/>
+      <div className="post">
+        <h1>{post.title}</h1>
+        <p>{post.body}</p>
+        <button onClick={()=>cretePost()}>Create Post</button>
+      </div>
     </div>
   )
 }
--- arquivos/app01/nome-do-seu-projeto/src/main.jsx
+++ arquivos/app01/nome-do-seu-projeto/src/main.jsx
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <RouterProvider router={router} />
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
 )
