2fd239a541c99e4f8c2a8c92bbc61b8f4fbae9c3
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-03-04 00:10:24 -0300

    aprendi axios

--- arquivos/app01/nome-do-seu-projeto/src/App.jsx
+++ arquivos/app01/nome-do-seu-projeto/src/App.jsx
@@ -9,39 +9,59 @@ export default function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(false);
 
+  const client = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com/posts'
+  });
+
   useEffect(
     ()=>{
-      const loadPost = () => {
+      const loadPost = async () => {
         setIsLoading(true);
-        axios.get('https://jsonplaceholder.typicode.com/posts/1')
-          .then(response => {
-            setPost(response.data)
-            setIsLoading(false);
-          })
-          .catch(response => {
-            setIsLoading(false);
-            setError(true);
-            console.log(response)
-          })
+        try{
+          const response = await client.get('/1');
+          setPost(response.data);
+          setIsLoading(false);
+        }catch(erro){
+          setIsLoading(false);
+          setError(true);
+          console.log(erro);
+        }
       }
 
       loadPost();
     },[]
   );
   
-  const cretePost = () => {
-    axios.post('https://jsonplaceholder.typicode.com/posts',{
+  const cretePost = async () => {
+    const response = await client.post('/' , {
       title: "Hello World!",
       body: "This is a new post."
-    })
-      .then((response) => {
-        setPost(response.data);
-      })
+    });
+
+    setPost(response.data);
   }
+
+  const updatePost = async () => {
+    const response = await client.put('/1' , {
+      title: 'Hello World',
+      body: 'This is an updated post.'
+    });
+
+    setPost(response.data);
+  }
+
+  const deletePost = async () => {
+    await client.delete('/1');
+    alert('Post removed');
+    setPost(null);
+  }
+
 
   if(error) {
     return <p>Error</p>
   } else if(isLoading) return <p>Loading</p>
+
+  if(!post) return <p>No post!</p>
  
 
   return (
@@ -50,6 +70,8 @@ export default function App() {
         <h1>{post.title}</h1>
         <p>{post.body}</p>
         <button onClick={()=>cretePost()}>Create Post</button>
+        <button onClick={()=>updatePost()}>Update Post</button>
+        <button onClick={()=>deletePost()}>Delete Post</button>
       </div>
     </div>
   )
