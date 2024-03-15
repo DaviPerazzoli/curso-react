f151cced876b7fceb4f243d35d29727bc63b7569
Davi Perazzoli <135984234+DaviPerazzoli@users.noreply.github.com>
2024-03-14 23:23:45 -0300

    aprendi Jest

--- arquivos/app01/app09/src/App.test.js
+++ arquivos/app01/app09/src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe('true is truthy and false is falsy', () => {
+  test('true is truthy', ()=>{
+    expect(true).toBeTruthy();
+  })
+  
+  test('false is falsy', () => {
+    expect(false).toBeFalsy();
+  })
+})
+
+
+test('1.35351322 + 2.0000123512 = 3.35352322', () => {
+  expect(1.35351322+2.0000123512).toBeCloseTo(3.35352322);
+})
